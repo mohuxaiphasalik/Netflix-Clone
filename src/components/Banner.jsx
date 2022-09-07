424b39c9edf0bd9b2054e992ec827c4b5ce8e9e5
Huzaifa Salik <huzaifasalik786@gmail.com>
2022-09-07 12:37:33 +0500

    Banner play button functionality added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import axios from "../axios";
 import requests from "../request";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 export default function Banner() {
+  var stringTruncate = function (str, length) {
+    if (str) {
+      var dots = str.length > length ? "..." : "";
+      return str.substring(0, length) + dots;
+    }
+  };
   const [movie, setMovie] = React.useState({});
+  const [trailerURL, setTrailerURL] = React.useState("");
+
   const img_base_url = "https://image.tmdb.org/t/p/original";
   React.useEffect(() => {
     axios
@@ -15,9 +25,30 @@ export default function Banner() {
         )
       );
   }, []);
-  console.log(movie);
+  function handleBannerClick() {
+    if (trailerURL) {
+      setTrailerURL("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name || "")
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerURL(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  }
+  const opts = {
+    height: "150",
+    width: "300px",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+      color: "#e50914",
+    },
+  };
   return (
-    <header>
+    <header className={trailerURL && "bigger-banner"}>
       <div className="banner">
         <img
           src={`${img_base_url}${movie?.backdrop_path}`}
@@ -28,14 +59,17 @@ export default function Banner() {
           <h1 className="title">
             {movie?.title || movie?.name || movie?.original_name}
           </h1>
-          <button className="banner-button play">
+          <button className="banner-button play" onClick={handleBannerClick}>
             <i className="fa-solid fa-play icon"></i>Play
           </button>
           <button className="banner-button list">
             {" "}
             <i className="fa-light fa-plus icon plus-icon"></i> Add to List
           </button>
-          <h5 className="desc">{movie.overview}</h5>
+          {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
+          {!trailerURL && (
+            <h5 className="desc">{stringTruncate(movie.overview, 150)}</h5>
+          )}
         </div>
       </div>
     </header>
